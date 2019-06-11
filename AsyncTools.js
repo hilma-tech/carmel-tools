2d42769cd0e5b81a2aa982f99ab059e199e1209b
Eran <eran.opalnet@gmail.com>
2019-06-11 13:37:26 +0300

    adds superFetch

--- AsyncTools.js
+++ AsyncTools.js
@@ -5,8 +5,33 @@ const AsyncTools = {
             return [null, data];
         })
             .catch(err => [err]);
-    }
-    
+    },
+
+    superFetch(url,payload=null){
+
+          let fPromise=payload==null ? fetch(url) : fetch(url,payload);
+
+          return new Promise((res,rej)=>{
+            
+            fPromise.then(r=>{
+              
+              if (r && r.ok===true){
+
+                r.json().then(re=>{
+
+                  res( [re,null] );
+              
+                })
+                .catch(err=>res( [null,err] ));
+
+              }else{
+                res( [null,"No response, check your network connectivity"]);
+              }
+
+            })
+            .catch(err=>{res( [null,err] );});
+        });    
+    }    
 }
 
 export default AsyncTools;
