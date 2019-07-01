9043fcb28d4d32b2816c892a9402feb125d9cce9
ReutSchremer <totireut@gmail.com>
2019-07-01 11:34:47 +0300

    new superfetch-> by reut and eliedaat

--- AsyncTools.js
+++ AsyncTools.js
@@ -1,37 +1,70 @@
 const AsyncTools = {
-    
-    to(promise) {
-        return promise.then(data => {
-            return [null, data];
+
+  to(promise) {
+    return promise.then(data => {
+      return [null, data];
+    })
+      .catch(err => [err]);
+  },
+
+  superFetchX(url, payload = null) {
+
+    let fPromise = payload == null ? fetch(url) : fetch(url, payload);
+
+    return new Promise((res, rej) => {
+
+      fPromise.then((r) => {
+
+        if (r && r.ok === true) {
+
+          r.json().then(re => {
+
+            res([re, null]);
+
+          })
+            .catch(err => res([null, err]));
+
+        } else {
+          console.log('error hy', r);
+          res([null, "No response, check your network connectivity"]);
+        }
+
+      })
+        .catch(err => { res([null, err]); });
+    });
+  },
+
+
+  parseJSON(response) {
+    return new Promise((resolve, reject) =>
+      response.json()
+        .then((json) => resolve({
+          status: response.status,
+          ok: response.ok,
+          json,
+        }))
+        .catch(error => { reject(error) })
+    );
+  },
+
+  superFetch(url, payload) {
+
+    let fPromise = payload == null ? fetch(url) : fetch(url, payload);
+
+    return new Promise((resolve, reject) => {
+      fPromise
+        .then(this.parseJSON)// this trys to parse- get origin error when you have one.
+        .then((response) => {
+          if (response.ok) {
+            return resolve([response.json, null]);
+          }
+          // extract the error from the server's json
+          return resolve([null, response.json]);
         })
-            .catch(err => [err]);
-    },
+        .catch((error) => resolve([null, "No response, check your network connectivity"]));
+    });
+  }
 
-    superFetch(url,payload=null){
-
-          let fPromise=payload==null ? fetch(url) : fetch(url,payload);
-
-          return new Promise((res,rej)=>{
-            
-            fPromise.then(r=>{
-              
-              if (r && r.ok===true){
-
-                r.json().then(re=>{
-
-                  res( [re,null] );
-              
-                })
-                .catch(err=>res( [null,err] ));
-
-              }else{
-                res( [null,"No response, check your network connectivity"]);
-              }
-
-            })
-            .catch(err=>{res( [null,err] );});
-        });    
-    }    
 }
 
 export default AsyncTools;
