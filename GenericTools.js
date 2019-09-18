0ce958ab3f7ec1f2c2e9430b0be146eab64c1179
ReutSchremer <totireut@gmail.com>
2019-09-18 09:19:46 +0300

    delete cookie func

--- GenericTools.js
+++ GenericTools.js
@@ -2,7 +2,7 @@ const GenericTools = {
   getCookieByKey(name) { // mv to tools/cookeis  getcookieByKey
     var value = "; " + document.cookie;
     var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
+    if (parts.length === 2) return parts.pop().split(";").shift();
   },
   deleteAllCookies() {
     var cookies = document.cookie.split(";");
@@ -12,6 +12,14 @@ const GenericTools = {
       var eqPos = cookie.indexOf("=");
       var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
       document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+  },
+  deleteCookieByKey(name, path = '/', domain = null) {
+    if (this.getCookieByKey(name)) {
+      document.cookie = name + "=" +
+        ((path) ? ";path=" + path : "") +
+        ((domain) ? ";domain=" + domain : "") +
+        ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
     }
   }
 }
