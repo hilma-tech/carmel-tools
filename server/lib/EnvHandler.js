7187ebccbf509fabe17b02d3205ee01d4c176593
shirarab <broomonster@gmail.com>
2020-02-27 15:19:29 +0200

    added more function that deel with environment to envhandler

--- server/lib/EnvHandler.js
+++ server/lib/EnvHandler.js
@@ -1,12 +1,25 @@
 require('dotenv').config()
 
 class EnvHandler {
-    getHostName () {
+    getHostName() {
         const hostName = process.env.NODE_ENV == 'production' ?
             process.env.REACT_APP_DOMAIN ? process.env.REACT_APP_DOMAIN : '.'
             : 'http://localhost:8080';
         return hostName;
     };
+
+    getEnv() { return process.env.NODE_ENV; }
+
+    isEnvLocal() {
+        let env = process.env.NODE_ENV;
+        if (env === "production" || env === "staging") return false;
+        return true;
+    }
+
+
+    isEnvStaging() { return process.env.NODE_ENV === "staging" ? true : false; }
+
+    isEnvProduction() { return process.env.NODE_ENV === "production" ? true : false; }
 }
 
 module.exports = new EnvHandler();
