2f688b162dec362bb633703ace04dfb2a405bfd2
maayankoren <nghieuri@gmail.com>
2020-03-04 15:32:09 +0200

    adding client-side hooks

---
+++ client/components/hooks/HooksFactory.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import HooksRepository from './HooksRepository'
+
+ class HooksFactory {
+    hooksRepository = null;
+    constructor() {
+        this.hooksRepository = new HooksRepository();
+        
+        let modulesList =require("./modulesList.json").modulesList;
+
+        let m = null;
+        modulesList.forEach((moduleName) => {
+
+            let moduleInstance = null;
+
+            try {
+                moduleInstance = require(`./../../../../${moduleName}/consts/HooksList`).default;
+                 new moduleInstance(this.hooksRepository).addHooks()
+
+            } catch (err) {
+                console.log("err", err)
+            }
+
+        })
+
+
+
+    }
+    getRepository() {
+        return this.hooksRepository;
+
+    }
+}
+export default new HooksFactory();
