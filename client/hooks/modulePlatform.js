aa665a2d2c106a13cfb5b19c78649db97f82845a
carmel6000 <shmuel.bendahan@carmel6000.amitnet.org>
2020-03-24 12:16:29 +0200

    fix bug

--- client/hooks/modulePlatform.js
+++ client/hooks/modulePlatform.js
@@ -1,5 +1,6 @@
-export default ModulePlatform = {
+const ModulePlatform = {
     auth_rn: require('./../../../auth/consts/HooksList_rn.js').default,
     auth_web: require('./../../../auth/consts/HooksList_web.js').default,
     auth_cordova: require('./../../../auth/consts/HooksList_cordova.js').default,
 }
+export default ModulePlatform;
