3a5106a6def850edecac5050e5eb4c6169959f3f
ReutSchremer <totireut@gmail.com>
2020-06-22 16:16:42 +0300

    localenv

--- server/lib/EnvHandler.js
+++ server/lib/EnvHandler.js
@@ -2,22 +2,22 @@ require('dotenv').config()
 
 class EnvHandler {
     getHostName() {
-	
-        
-	const hostName = !this.isEnvLocal() ?
+
+
+        const hostName = !this.isEnvLocal() ?
             process.env.REACT_APP_DOMAIN ? process.env.REACT_APP_DOMAIN : '.'
             : 'http://localhost:8080';
 
         console.log("HOST", hostName);
-	console.log("ENVS", process.env.NODE_ENV);
-	return hostName;
+        console.log("ENVS", process.env.NODE_ENV);
+        return hostName;
     };
 
     getEnv() { return process.env.NODE_ENV; }
 
     isEnvLocal() {
         let env = process.env.NODE_ENV;
-        if (env === "" || env === "development") return true;
+        if (!env || env === "" || env === "development") return true;
         return false;
     }
 
