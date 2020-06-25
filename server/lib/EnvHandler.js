5048e61ec6fc17fecb641f8d494af1b23fd2fd4e
shirarab <broomonster@gmail.com>
2020-06-25 09:49:57 +0300

    env handler is a static class

--- server/lib/EnvHandler.js
+++ server/lib/EnvHandler.js
@@ -1,29 +1,27 @@
 require('dotenv').config()
 
 class EnvHandler {
-    getHostName() {
-
-
+    static getHostName() {
         const hostName = !this.isEnvLocal() ?
             process.env.REACT_APP_DOMAIN ? process.env.REACT_APP_DOMAIN : '.'
             : 'http://localhost:8080';
 
-        console.log("HOST", hostName);
-        console.log("ENVS", process.env.NODE_ENV);
+        // console.log("HOST", hostName);
+        // console.log("ENVS", process.env.NODE_ENV);
         return hostName;
     };
 
-    getEnv() { return process.env.NODE_ENV; }
+    static getEnv() { return process.env.NODE_ENV; }
 
-    isEnvLocal() {
+    static isEnvLocal() {
         let env = process.env.NODE_ENV;
         if (!env || env === "" || env === "development") return true;
         return false;
     }
 
-    isEnvStaging() { return process.env.NODE_ENV === "staging" ? true : false; }
+    static isEnvStaging() { return process.env.NODE_ENV === "staging" ? true : false; }
 
-    isEnvProduction() { return process.env.NODE_ENV === "production" ? true : false; }
+    static isEnvProduction() { return process.env.NODE_ENV === "production" ? true : false; }
 }
 
-module.exports = new EnvHandler();
+module.exports = EnvHandler;
